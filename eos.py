6baba47904914e81eab0c7cc53d6d0bbed65223d
michael-cowan <mcowan92@gmail.com>
2018-03-04 22:36:27 -0500

    added Redlich Kwong EOS to eos.py

--- eos.py
+++ eos.py
@@ -42,6 +42,27 @@ class PengRobinsonEOS(EOS):
         return p - term1 + term2
 
 
+class RedlichKwongEOS(EOS):
+    """
+        0 = p - [RT / (v - b)] + [a / (sqrt(T) v (v + b))]
+    """
+    name = 'Redlich Kwong EOS'
+
+    def __init__(self, pc, Tc, R=None):
+        if R:
+            self.R = float(R)
+
+        self.pc = pc
+        self.Tc = Tc
+
+    def eos(self, p, v, T):
+        a = (0.42748 * self.R**2 * self.Tc**(5/2.)) / self.pc
+        b = (0.08664 * self.R * self.Tc) / self.pc
+        term1 = self.R * T / (v - b)
+        term2 = a / (np.sqrt(T) * v * (v + b))
+        return p - term1 + term2
+
+
 class VanDerWaalsEOS(EOS):
     """
         (p + a / v^2)(v - b) = RT
@@ -74,12 +95,14 @@ if __name__ == '__main__':
     # initialize gas EOS objects
     nh3_ideal = IdealGasEOS()
     nh3_peng = PengRobinsonEOS(pc, Tc, omega)
+    nh3_red = RedlichKwongEOS(pc, Tc)
     nh3_vdw = VanDerWaalsEOS(pc, Tc)
 
     # state dict is returned
     stp_ideal = nh3_ideal.get_state(p=p, T=T)
     stp_peng = nh3_peng.get_state(p=p, T=T)
+    stp_red = nh3_red.get_state(p=p, T=T)
     stp_vdw = nh3_vdw.get_state(p=p, T=T)
 
     # last calculated state dict is also set as atttribute
-    [print(e.state) for e in [nh3_ideal, nh3_peng, nh3_vdw]]
+    [print(e.state) for e in [nh3_ideal, nh3_peng, nh3_red, nh3_vdw]]
