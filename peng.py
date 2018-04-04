9852bd18f50708997be45b2e8add590812ebe519
michael-cowan <mcowan92@gmail.com>
2018-04-04 18:28:15 -0400

    fixed peng to filter out imaginary and negative volume roots

--- peng.py
+++ peng.py
@@ -17,7 +17,7 @@ class PengRobinsonEOS():
         self.Tc = Tc
         self.kappa = 0.37464 + 1.54226 * omega - 0.26992 * omega**2
 
-    def solve_v(self, p, T, root='liquid'):
+    def calc_v(self, p, T, root='liquid'):
         """ c1 * v^3 + c2 * v^2 + c3 * v + c4 = 0 """
         a = 0.45724 * (self.R * self.Tc)**2 / self.pc
         b = 0.07780 * self.R * self.Tc / self.pc
@@ -28,8 +28,19 @@ class PengRobinsonEOS():
         c2 = p * b - self.R * T
         c3 = a * alpha - 3 * p * b**2 - 2 * self.R * T * b
         c4 = p * b**3 - a * alpha * b + self.R * T * b**2
-        vs = np.roots([c1, c2, c3, c4])
-        vs = [i for i in vs if i > 0]
+        roots = np.roots([c1, c2, c3, c4])
+
+        # filter negative and complex numbers
+        vs = []
+        for val in roots:
+            if val <= 0:
+                continue
+            elif isinstance(val, complex):
+                if val.imag:
+                    continue
+                else:
+                    val = val.real
+            vs.append(val)
 
         # return appropriate root depending on phase
         return min(vs) if root.lower() in 'liquid' else max(vs)
@@ -37,12 +48,14 @@ class PengRobinsonEOS():
 if __name__ == '__main__':
     # Ammonia (NH_3) props
     pc = 113.53  # bar
-    Tc = 405.4  # K
+    Tc = 405.4   # K
     omega = 0.257
 
-    # standart pressure and temperature
-    p = 1.01325
-    T = 273.15
+    # standard pressure and temperature
+    p = 1.01325  # bar
+    T = 273.15   # K
 
     peng = PengRobinsonEOS(pc, Tc, omega)
-    x = peng.solve_v(p, T)
+
+    # calculates volume in L
+    vol = peng.calc_v(p, T, 'vapor')
