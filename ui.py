e3ef8d471759950e5920e7ed05ff0232e1ed8b64
michael-cowan <mcowan92@gmail.com>
2018-04-05 13:59:04 -0400

    peng tab is now dynamically calcs volume

--- ui.py
+++ ui.py
@@ -28,8 +28,9 @@ class PengUI:
         tab = QWidget()
         lay = QGridLayout()
 
-        font = QFont('SansSerif', 16)
+        # font = QFont('SansSerif', 14)
         cen = QtCore.Qt.AlignCenter
+        right = QtCore.Qt.AlignRight
 
         self.p_lbl = QLabel('Pressure (bar):')
         self.p = QLineEdit(text=str(stp_p))
@@ -41,58 +42,55 @@ class PengUI:
 
         self.root_lbl = QLabel('Phase:')
         self.liq = QRadioButton('Liquid')
+        self.liq.setFixedWidth(140)
         self.vap = QRadioButton('Vapor')
+        self.vap.setFixedWidth(100)
         self.vap.setChecked(True)
-        self.V_lbl = QLabel('Volume (L / mol): ')
+        self.V_lbl = QLabel('Volume (L / mol):')
         self.V = QLabel(text='%.3e L' % stp_v)
         self.V.setAlignment(cen)
-        self.msg = QLabel()
-
-        self.calc = QPushButton('Calculate Volume')
-        self.calc.setFixedHeight(100)
 
         for w in [self.p_lbl, self.p, self.T_lbl,
                   self.T, self.root_lbl, self.liq,
-                  self.vap, self.V_lbl, self.V,
-                  self.msg, self.calc]:
-            w.setFont(font)
+                  self.vap, self.V_lbl, self.V]:
+            pass  # w.setFont(font)
 
         root = QHBoxLayout()
         root.addWidget(self.liq)
         root.addWidget(self.vap)
+        root.setAlignment(cen)
 
-        lay.addWidget(self.p_lbl, 1, 0)
-        lay.addWidget(self.p, 1, 2)
-        lay.addWidget(self.T_lbl, 2, 0)
-        lay.addWidget(self.T, 2, 2)
-        lay.addWidget(self.root_lbl, 3, 0)
-        lay.addLayout(root, 3, 2)
-        lay.addWidget(self.calc, 4, 2)
-        lay.addWidget(self.V_lbl, 5, 0)
-        lay.addWidget(self.V, 5, 2)
-        lay.addWidget(self.msg, 6, 1)
+        lay.setColumnStretch(1, 2)
+        lay.addWidget(self.p_lbl, 0, 0, right)
+        lay.addWidget(self.p, 0, 1)
+        lay.addWidget(self.T_lbl, 1, 0, right)
+        lay.addWidget(self.T, 1, 1)
+        lay.addWidget(self.root_lbl, 2, 0, right)
+        lay.addLayout(root, 2, 1)
+        lay.addWidget(self.V_lbl, 3, 0, right)
+        lay.addWidget(self.V, 3, 1)
 
         lay.setVerticalSpacing(50)
-        lay.setContentsMargins(100, 50, 100, 50)
+        lay.setContentsMargins(20, 100, 20, 350)
         tab.setLayout(lay)
 
         @QtCore.pyqtSlot()
-        def calc_click():
+        def calculate():
             try:
                 p = float(self.p.text())
                 T = float(self.T.text())
             except:
-                self.msg.setText('Pressure and temperature must be numbers')
+                self.V.setText('')
             else:
-                if p <= 0:
-                    self.msg.setText('Invalid pressure.')
-                elif T <= 0:
-                    self.msg.setText('Invalid temperature.')
+                if p <= 0 or T <= 0:
+                    self.V.setText('')
                 else:
                     root = 'vapor' if self.vap.isChecked() else 'liquid'
                     self.V.setText('%.3e L' % (peng.calc_v(p, T, root)))
 
-        self.calc.clicked.connect(calc_click)
+        self.p.textChanged.connect(calculate)
+        self.T.textChanged.connect(calculate)
+        self.vap.toggled.connect(calculate)
 
         self.w.addTab(tab, 'Peng Robinson EOS')
 
